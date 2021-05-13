1b93019aabf9d2a56238c8d8ce262f30410e78af
kavisha <kavisha_jg@yahoo.com>
2021-05-13 11:47:38 +0530

    Initial Commit

---
+++ Shared/MultipleAppIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleAppIconApp.swift
+//  Shared
+//
+//  Created by Kavisha Sonaal on 13/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MultipleAppIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
