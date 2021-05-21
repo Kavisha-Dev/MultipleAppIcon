b2fd5757109618bf5fb5513f7cd957a8293b5ec6
kavisha <kavisha_jg@yahoo.com>
2021-05-21 13:22:27 +0530

    Alternate icon code committed

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+/*
+ Reddit user mentioned that the code works on a restart - https://www.reddit.com/r/SwiftUI/comments/k4k5o5/how_do_i_allow_the_user_to_change_app_icons/gyt0irz/
+ It works fine on a Simulator anyways.
+ */
 struct ContentView: View {
+    
+    // Refer to details mentioned in AppIconView saying why this is commented.
+    //@EnvironmentObject var iconSettings: IconNames
+    
+    @StateObject private var iconSettings : IconNames = IconNames()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            List {
+                Section(header: Text("Appearance")) {
+                    NavigationLink(destination: AppIconView()) {
+                        Label("Choose another App Icon", systemImage: "square.grid.2x2").foregroundColor(Color.primary)
+                    }
+                    
+                    NavigationLink(destination: AppIconPickerView()) {
+                        Label("Choose another App Icon", systemImage: "square.grid.2x2").foregroundColor(Color.primary)
+                    }
+                    
+                    NavigationLink(destination: AppIconStateView()) {
+                        Label("Choose another App Icon", systemImage: "square.grid.2x2").foregroundColor(Color.primary)
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -18,4 +44,113 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct AppIconStateView: View  {
+    
+    @StateObject private var iconSettings : IconNames = IconNames()
+    
+    @State var chosenIconName : String?
+    
+    var body: some View {
+        
+        /// The size of iconNames is 3 due to setting it as [nil] in the AppDelegate. This is done so that the default app icon will also be displayed in this list.
+        List(self.iconSettings.iconNames, id: \.self) { item in
+            
+            HStack {
+                HStack(spacing: 15) {
+                    // Still blurred image
+                    Image(uiImage: UIImage(named: item ?? "AppIcon") ?? UIImage())
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                        .cornerRadius(8)
+                    
+                    Image(uiImage: UIImage(named: item ?? "AppIcon_01") ?? UIImage())
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                        .cornerRadius(8)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        if item == nil {
+                            Text("Default")
+                            ExternalLinkView(displayName: "@sonaal", clickableLink: "http://twitter.com/sonaal")
+                        } else {
+                            if let i = item,
+                               let fileName = K.fileNameDisplayName[i],
+                               let displayName = K.displayNameDisplayLink[i],
+                               let clickableLink = K.nameClickableLink[i] {
+                                
+                                Text(fileName)
+                                
+                                ExternalLinkView(displayName: displayName, clickableLink: clickableLink)
+                                
+                            } else {
+                                // error on fetching item name. cannot tap
+                            }
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                if item == self.chosenIconName {
+                    Image(systemName: "checkmark.circle").foregroundColor(Color.accentColor)
+                }
+            }
+            .padding([.top, .bottom], 10)
+            .contentShape(Rectangle())
+            .onTapGesture {
+                if item != self.chosenIconName {
+                    UIApplication.shared.setAlternateIconName(item) { error in
+                        // TODO: Change this!
+                        if let error = error {
+                            print(error.localizedDescription)
+                        } else {
+                            print("Success! You have changed the app icon to = \(item)")
+                            print(self.iconSettings.iconNames)
+                            
+                            self.chosenIconName = item
+                            self.iconSettings.currentIconName = item
+                        }
+                    }
+                }
+            }
+            // Set the chosenIndex here within the row. IMP: Set on link's onAppear ONLY. Doesnot work outside.
+            .onAppear() {
+                //self.chosenIndex = self.iconSettings.currentIndex
+                //self.chosenIconName = self.iconSettings.currentIconName
+                self.chosenIconName = UIApplication.shared.alternateIconName
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle("Choose Icon")
+    }
+}
+
+struct K {
+    
+    // Add more icons here
+    static var fileNameDisplayName: Dictionary<String, String> = [
+        "MNM_01":"Forks",
+        "MNM_02":"Funky Forks",
+        "MNM_Sonaal01" : "Pretty Veg"
+    ]
+    //linkClickableSafariLink
+    static var displayNameDisplayLink: Dictionary<String, String> = [
+        "Default": "@sonaal",
+        "MNM_01":"www.crawfordandjohn.com",
+        "MNM_02":"www.crawfordandjohn.com"
+    ]
+    
+    static var nameClickableLink: Dictionary<String, String> = [
+        "Default": "http://twitter.com/sonaal",
+        "MNM_01":"https://www.crawfordandjohn.com",
+        "MNM_02":"https://www.crawfordandjohn.com"
+    ]
 }
--- Shared/MultipleAppIconApp.swift
+++ Shared/MultipleAppIconApp.swift
@@ -12,6 +12,45 @@ struct MultipleAppIconApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                //.environmentObject(IconNames())
+        }
+    }
+}
+
+
+// MARK: - ALTERNATE ICONS
+
+class IconNames: ObservableObject {
+    
+    /// Setting nil at the first index to support the primary icon to be displayed. i.e when the iconname is nil, the primary icon will be set.
+    /// the first element of the iconNames array is set as nil as an indicator of the primary app icon.
+    var iconNames: [String?] = [nil]
+    @Published var currentIndex = 0
+    
+    @Published var currentIconName: String?
+    
+    init() {
+        getAlternateIconNames()
+        
+        self.currentIconName = UIApplication.shared.alternateIconName
+        
+        /*if let currentIcon = UIApplication.shared.alternateIconName {
+            self.currentIndex = iconNames.firstIndex(of: currentIcon) ?? 0
+        }*/
+    }
+    
+    func getAlternateIconNames() {
+        if let icons = Bundle.main.object(forInfoDictionaryKey: "CFBundleIcons") as? [String: Any],
+           let alternateIcons = icons["CFBundleAlternateIcons"] as? [String: Any] {
+            for (_, value) in alternateIcons {
+                guard let iconList = value as? Dictionary<String,Any> else { return }
+                guard let iconFiles = iconList["CFBundleIconFiles"] as? [String] else { return }
+                guard let icon = iconFiles.first else { return }
+                
+                iconNames.append(icon)
+            }
+            print("iconNames \(iconNames)")
+            print("\n")
         }
     }
 }
